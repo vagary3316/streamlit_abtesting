7ccb5dd5f16e777e7591d5e9a98b59a05ca33dc1
Selina <selina.chen@mdfinstruments.com>
2025-04-23 23:48:59 +0800

    inital

---
+++ 1_abtesting.py
@@ -0,0 +1,36 @@
+import kagglehub
+from kagglehub import KaggleDatasetAdapter
+import openpyxl
+import pandas as pd
+import streamlit as st
+
+## streamlit_setting
+st.set_page_config(layout="wide",
+                   page_title="AB Testing Dataset from Kaggle",
+                   page_icon=":Chart:")
+
+#Streamlit Header and Subheader
+st.header('Welcome to my project - AB Testing Data Analysis')
+st.text("""
+Kaggle - AB Testing Dataset
+(https://www.kaggle.com/datasets/cagataytuylu/ab-testing)
+
+This page shows the data processing and every analysis steps.
+""")
+
+
+file_path = "ab_testing.xlsx"
+
+# Load the Control Group from Kaggle
+con_df = kagglehub.load_dataset(
+  KaggleDatasetAdapter.PANDAS,
+  "cagataytuylu/ab-testing",
+  file_path,
+  pandas_kwargs={'sheet_name':'Control Group'})
+
+# Load the Test Group from Kaggle
+test_df = kagglehub.load_dataset(
+  KaggleDatasetAdapter.PANDAS,
+  "cagataytuylu/ab-testing",
+  file_path,
+  pandas_kwargs={'sheet_name':'Test Group'})
